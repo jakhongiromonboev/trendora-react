de5e57be6c1848a093f7e5d00f16bf6180f9d5d7
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-11-23 20:59:30 +0900

    Initial commit - Trendora React setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 style={{ padding: "20px", background: "gold" }}>Trendora React</h1>
+      <div className="card">
+        <button
+          style={{ padding: "10px", fontSize: "20px", borderRadius: "12px" }}
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
