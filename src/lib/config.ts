2cad13a9add0edb2dac8aab5208bb3879d4a39cf
jakhongiromonboev <jakhongiromonboev@gmail.com>
2025-11-29 18:32:07 +0900

    feat: develop bsket and sweet error handling & error realted messages

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,9 @@
 export const serverApi: string = `${import.meta.env.VITE_APP_API_URL}`;
+
+export const Messages = {
+  error1: "Something went wrong!",
+  error2: "Please login first!",
+  error3: "Please fulfill all inputs!",
+  error4: "Message is empty!",
+  error5: "Only images with jpeg,jpg,png format allowed!",
+};
